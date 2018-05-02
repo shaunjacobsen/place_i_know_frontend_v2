348c80e277f2c096e407739e173ca06ade06ced0
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-05-02 17:27:56 -0500

    Header as primary navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Icon } from 'antd';
 import { startSignOut } from './../../actions/auth';
 import UserInfo from './UserInfo';
 
-export const Header = props => (
-  <div className="header">
-    <div className="header__content">
-      <div className="header__title">
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div className="header__content">
+          <Link to="/home">
+            <img src="/images/logo.png" alt="Place I Know" className="header__logo" />
+          </Link>
+          <div className="header__nav">
+            <Link to="/home">
+              <div className="header__nav-element">
+                <span>
+                  <Icon type="home" />&nbsp;Home
+                </span>
+              </div>
+            </Link>
+            <Link to="/home">
+              <div className="header__nav-element">
+                <span className="active">
+                  <Icon type="compass" />&nbsp;Trips
+                </span>
+              </div>
+            </Link>
+            <Link to="/chat">
+              <div className="header__nav-element">
+                <span>
+                  <Icon type="message" />&nbsp;Chat
+                </span>
+              </div>
+            </Link>
+          </div>
+          <UserInfo />
+        </div>
       </div>
-      <UserInfo />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
