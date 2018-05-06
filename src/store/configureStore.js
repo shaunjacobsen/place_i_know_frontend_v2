2005435b2938eeeabbf7e81a542fbfc0fa633749
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2018-05-06 17:01:59 -0500

    Restructuring redux state

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from './../reducers/auth';
 import tripReducer from './../reducers/trip';
-import activeTripReducer from './../reducers/activeTrip';
+import itineraryReducer from './../reducers/itinerary';
+import eventsReducer from './../reducers/events';
+import daysReducer from './../reducers/days';
+import placesReducer from './../reducers/places';
+import accommodationGroupsReducer from './../reducers/accommodationGroups';
+import accommodationReducer from './../reducers/accommodation';
 import mapReducer from './../reducers/map';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,7 +17,14 @@ export default () => {
     combineReducers({
       auth: authReducer,
       trips: tripReducer,
-      activeTrip: activeTripReducer,
+      activeTrip: {
+        itinerary: itineraryReducer,
+        events: eventsReducer,
+        days: daysReducer,
+        places: placesReducer,
+        accommodationGroups: accommodationGroupsReducer,
+        accommodation: accommodationReducer,
+      },
       map: mapReducer,
     }),
     composeEnhancers(applyMiddleware(thunk))
